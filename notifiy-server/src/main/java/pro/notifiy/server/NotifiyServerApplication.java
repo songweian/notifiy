0acd24555b9f85b0543211aa66fe7cfb522e325e
weian404 <weian404@gmail.com>
2023-09-21 22:38:41 +0800

    init

---
+++ notifiy-server/src/main/java/pro/notifiy/server/NotifiyServerApplication.java
@@ -0,0 +1,13 @@
+package pro.notifiy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifiyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifiyServerApplication.class, args);
+	}
+
+}
---
+++ notifiy-server/src/test/java/pro/notifiy/server/NotifiyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.notifiy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifiyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
